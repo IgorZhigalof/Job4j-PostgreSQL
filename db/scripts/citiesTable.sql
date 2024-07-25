43788e33873586294d56aaaffdf629a3329fdda8
Игорь <hardplayer455@gmail.com>
2024-07-25 16:58:13 +0300

    0. Что такое база данных. [#6860]

---
+++ db/scripts/citiesTable.sql
@@ -0,0 +1,6 @@
+create table cities (
+	id serial primary key,
+	name varchar(255),
+	population int,
+	area float4
+);
---
+++ db/scripts/insertScript.sql
@@ -0,0 +1,1 @@
+insert into cities(name, area, population) values('Moscow', 2.511, 13000000);
